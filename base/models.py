b342ac4dd03bf49595ee223a24c3c42e5dccb431
Kaan Güler <glerkaan@yahoo.com>
2023-05-15 23:08:06 +0300

    Search and styling

--- base/models.py
+++ base/models.py
@@ -11,6 +11,8 @@ class Task(models.Model):
     complete = models.BooleanField(default=False)
     created=models.DateTimeField(auto_now_add=True)
 
+
+
     def __str__(self):
         return self.title
 
--- base/views.py
+++ base/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render,redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -49,13 +51,29 @@ class TaskList(LoginRequiredMixin,ListView):
         context=super().get_context_data(**kwargs)
         context["tasks"] = context["tasks"].filter(user=self.request.user)
         context["count"] = context["tasks"].filter(complete=False).count()
+        context["total"]=context["tasks"].count()
+        if(context["total"]!=0):
+            context["percantage"]=(context["count"]/context["total"])*100
+
+        search_input=self.request.GET.get("search-area")or ""
+        if search_input:
+            context["tasks"] = context["tasks"].filter(title__startswith=search_input)
+
+        context["search_input"]=search_input;
+
+
+
         return context
+
+
+
 
 
 class TaskDetail(LoginRequiredMixin,DetailView):
     model = Task
     context_object_name = "task"
     template_name = 'base/task.html'
+
 
 class TaskCreate(LoginRequiredMixin,CreateView):
     model = Task
@@ -70,9 +88,11 @@ class TaskUpdate(LoginRequiredMixin,UpdateView):
     model = Task
     fields = ["title", "description", "complete"]
     success_url = reverse_lazy("tasks")
+    model.created=datetime.datetime.now()
 
 class DeleteView(LoginRequiredMixin,DeleteView):
     model = Task
+    model.created=datetime.date.max.year
     context_object_name = "task"
     success_url = reverse_lazy("tasks")
 
